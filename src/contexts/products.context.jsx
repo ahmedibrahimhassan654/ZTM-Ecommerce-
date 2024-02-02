e8ff121af04edf18bdccabfbec8bdfc954d489cf
ahmedibrahimhassan654 <ahmedibrahimhassan654@gmail.com>
2024-02-02 20:51:48 +0200

    114. Products Context

---
+++ src/contexts/products.context.jsx
@@ -0,0 +1,27 @@
+import { createContext, useState } from "react";
+
+import PRODUCTS from "../shop-data.json";
+// as the actual value you want to access
+export const ProductsContext = createContext({
+  //initial value for our context
+  products: [],
+});
+
+//act as component cover the heigh level component which is App compnent
+export const ProductsProvider = ({ children }) => {
+  //store the user object inside current user state
+  const [products, setproducts] = useState(PRODUCTS);
+  const value = {
+    products,
+  };
+
+  console.log(
+    "final value object that witll inserted to the user provider",
+    value
+  );
+  return (
+    <ProductsContext.Provider value={value}>
+      {children}
+    </ProductsContext.Provider>
+  );
+};
--- src/routes/shop.js/Shop.jsx
+++ src/routes/shop.js/Shop.jsx
@@ -1,7 +1,9 @@
-import React from "react";
-import PRODUCTS from "../../shop-data.json";
-const Shop = (props) => {
-  return PRODUCTS.map(({ name, id, imageUrl, price }) => (
+import React, { useContext } from "react";
+import { ProductsContext } from "../../contexts/products.context";
+const Shop = () => {
+  const { products } = useContext(ProductsContext);
+
+  return products.map(({ name, id, imageUrl, price }) => (
     <div key={id}>
       <h1>{name}</h1>
     </div>
