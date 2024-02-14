dee084758e337dfed20e227a84fbbccfea8b82d7
ahmedibrahimhassan654 <ahmedibrahimhassan654@gmail.com>
2024-02-14 14:06:15 +0200

    142. Last Touches

--- src/components/form-input/form-input.style.jsx
+++ src/components/form-input/form-input.style.jsx
@@ -1,12 +1,12 @@
 import styled, { css } from "styled-components";
 
-const subColor = "grey";
-const mainColor = "black";
+const subColor = "green";
+const mainColor = "blue";
 
 const shrinkLabelStyles = css`
   top: -14px;
   font-size: 12px;
-  color: blue;
+  color: ${mainColor};
 `;
 
 export const FormInputLabel = styled.label`
