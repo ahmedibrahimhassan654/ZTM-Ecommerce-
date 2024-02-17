4e1e82cb0caa861ae1a1d60ed6b2987df5060478
ahmedibrahimhassan654 <ahmedibrahimhassan654@gmail.com>
2024-02-18 01:15:12 +0200

    154. React-Redux: Setting Up Our Store

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,11 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 
+import { rootreducer } from "./root-reducer";
 
+// root reducer
+const middelware = [logger];
 
-// root reducer 
+const composedEnhancer = compose(applyMiddleware(...middelware));
+
+export const store = createStore(rootreducer, undefined, composedEnhancer);
